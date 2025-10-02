8533e379a6cf175d4dfa969765a856e2c09d22c2
Otar Davitashvili <okupara123@gmail.com>
2025-10-02 22:35:34 +0400

    initial commit

---
+++ src/app/pages/seller-profile/seller-profile.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-seller-profile',
+  imports: [],
+  templateUrl: './seller-profile.html',
+  styleUrl: './seller-profile.scss'
+})
+export class SellerProfile {
+
+}
---
+++ src/app/shared/components/rating-star/rating-star.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rating-star',
+  imports: [],
+  templateUrl: './rating-star.html',
+  styleUrl: './rating-star.scss'
+})
+export class RatingStar {
+
+}
